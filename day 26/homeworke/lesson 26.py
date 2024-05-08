5a2e57455d25fac51076d103fcea567cc0cf7a30
gabrielgvinashvili <gabrielgvinashvili@gmail.com>
2024-05-08 21:57:21 +0400

    commit

---
+++ day 26/homeworke/lesson 26.py
@@ -0,0 +1,14 @@
+user_question = input("swavlob shen goashi?:   ")
+print(user_question)
+
+question = "ara"
+
+
+if user_question == "ki":
+    group = input("romel jgufshi xar?:  ")
+if group == "13":
+    teacher = input("batoni gabrieli gaswavlis?:  ")
+if teacher == "ki":
+    user_question2 = input("sen namdvilad goashi swavlob da matla batoni gabrieli gaswavlis")
+elif teacher == "ara":
+    print("zalian cudi semodi goashi")
