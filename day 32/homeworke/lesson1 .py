2102b6f421fb948867e041894cbdd354e6a3e8e4
gabrielgvinashvili <gabrielgvinashvili@gmail.com>
2024-05-26 14:39:44 +0400

    (commit)

---
+++ day 32/homeworke/lesson1 .py
@@ -0,0 +1,7 @@
+goa = "goa is  the best"
+add =[1,2,3,4,5]
+best_goa = ""
+for char in range(len(goa)):
+    if goa[char] != " " :
+        best_goa += goa[char]
+print(best_goa)
---
+++ day 32/homeworke/lesson2.py
@@ -0,0 +1,7 @@
+goa = "goa is is cool on the novator"
+add =[1,2,3,4,5,6,7,8]
+goa_is_best = ""
+for char in range(len(goa)):
+    if goa[char] != " " :
+       goa_is_best += goa[char]
+print(goa_is_best)
