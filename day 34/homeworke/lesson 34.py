41616dbfc671efb5ea6b47c026457a6dfe08d10b
gabrielgvinashvili <gabrielgvinashvili@gmail.com>
2024-05-31 12:58:36 +0400

    commit

---
+++ day 34/homeworke/lesson 34.py
@@ -0,0 +1,4 @@
+goa =["giviko kavrishvili", "giorgi vaniashvili", "anastasia khvedelidze", "andria alavidze", "andria iakobishvili", "andria korchilava", "andria samsonizde",
+"andria shanava", "beridze mate", "dato miribiani" "deme biganashvili", "erekle karagozishvili", "gabriel gvinashvili", "gio alada", "luka chakhvashvili", "luka natroshvili"
+"mate kotorashvili", "mate tolordava", "mekaluashvili andria", " nika chunashvili"]
+print(goa[9:19])
